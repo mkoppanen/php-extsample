e1f34583246d9a0e3e5ed658f1d78efe66868e45
Mikko <mikko@kuut.io>
2013-10-20 00:50:33 +0800

    Added example of array input parameter and returning an array

--- extsample.c
+++ extsample.c
@@ -51,8 +51,9 @@ PHP_METHOD(extsample, __construct)
 
 	/* Parse one string parameter, name. Notice that name_len has to be int, not long 
 		otherwise it causes problems on platforms where long and int are different size
+		| makes parameters after it optional
 	*/
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &name, &name_len) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|s", &name, &name_len) == FAILURE) {
 		/*
 			Notice that if parameter parsing has failed in constructor then we return here
 			any possible initialisation that happens after this would not be executed and
@@ -65,10 +66,12 @@ PHP_METHOD(extsample, __construct)
 	/* This is how you get access to the internal structure allocated in php_extsample_object_new */
 	intern = (php_extsample_object *) zend_object_store_get_object(getThis() TSRMLS_CC);
 
-	/* We store the name into the struct using estrdup
+	/* We store the name into the struct using estrdup if it's set
 	   It's essentially same as strdup but uses PHP memory management
 	 */
-	intern->name = estrdup (name);
+	if (name_len) {
+		intern->name = estrdup (name);
+	}
 }
 /* }}} */
 
@@ -105,6 +108,146 @@ PHP_METHOD(extsample, getname)
 	   a dangling pointer inside the object
 	*/
 	RETURN_STRING(intern->name, 1);
+}
+/* }}} */
+
+static
+char *s_get_zval_type (zval *value)
+{
+	switch (Z_TYPE_P(value)) {
+		case IS_NULL:
+		return estrdup ("IS_NULL");
+
+		case IS_LONG:
+		return estrdup ("IS_LONG");
+
+		case IS_DOUBLE:
+		return estrdup ("IS_DOUBLE");
+
+		case IS_BOOL:
+		return estrdup ("IS_BOOL");
+
+		case IS_ARRAY:
+		return estrdup ("IS_ARRAY");
+
+		case IS_OBJECT:
+		return estrdup ("IS_OBJECT");
+
+		case IS_STRING:
+		return estrdup ("IS_STRING");
+
+		case IS_RESOURCE:
+		return estrdup ("IS_RESOURCE");
+
+		case IS_CONSTANT:
+		return estrdup ("IS_CONSTANT");
+
+		case IS_CONSTANT_ARRAY:
+		return estrdup ("IS_CONSTANT_ARRAY");
+
+#if defined(IS_CALLABLE)
+		case IS_CALLABLE:
+		return estrdup ("IS_CALLABLE");
+#endif
+
+		default:
+			return estrdup ("IS_UNKNOWN");
+	}
+}
+
+#if (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION < 3)
+static int s_get_element_type(zval **ppzval, int num_args, va_list args, zend_hash_key *hash_key)
+{
+	/* The older version doesn't pass TSRMLS_DC in arguments so we need to call this macro*/
+	TSRMLS_FETCH();
+#else
+static int s_get_element_type(zval **ppzval TSRMLS_DC, int num_args, va_list args, zend_hash_key *hash_key)
+{
+#endif
+	zval *return_value;
+	char *type_name;
+
+	/* Pick up the return_value from the va_list */
+	return_value = va_arg(args, zval *);
+
+	/*
+		Next, ppzval contains our current array item. What we want to do is to check the type
+		and add it to return_value with same key. So for example:
+		array (1 => 'hello', 'foo' => 2) would return array (1 => 'IS_STRING', 'foo' => IS_LONG)
+	*/
+	type_name = s_get_zval_type (*ppzval);
+
+	/* If key length is 0 it means that the index is integer rather than associative */
+	if (!hash_key->nKeyLength) {
+		/* If it's an integer key, this is how you can access it
+			The last parameter is whether we copy the string to array.
+			In this case we don't copy, because it's coming from estrdup
+			above and we have no use for it later. Hence we give the ownership
+			away and it will be freed when return value goes out of scope
+		*/
+		add_index_string(return_value, hash_key->h, type_name, 0);
+
+	} else {
+		/*
+			In this case we have a string key, let's add it to the array
+			using add_assoc_string instead of add_index_string. Also, like
+			with integer key don't copy string
+		*/
+		add_assoc_string(return_value, hash_key->arKey, type_name, 0);
+	}
+
+	/* This return value continues iteration. We could also return:
+		ZEND_HASH_APPLY_REMOVE - Continues iteration but removes current element
+		ZEND_HASH_APPLY_STOP - Stop iteration
+	*/
+	return ZEND_HASH_APPLY_KEEP;
+}
+
+/* {{{ proto string ExtSample::arrayValueTypes(array $arr)
+    Handling array input parameter. Takes array as parameter and returns an array containing 
+	type for each key
+*/
+PHP_METHOD(extsample, arrayvaluetypes)
+{
+	/* The array will be given as zval ptr */
+	zval *array;
+
+	/* "a" to receive array parameter */
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "a", &array) == FAILURE) {
+		return;
+	}
+
+	/*
+		There are two main ways to iterate over an array: 
+		Callback based iteration and iterating by getting the HashTable inside the zval * and using a for loop
+	*/
+
+	/*
+		This method returns an array so we need to initialise the return value to array
+		'return_value' is a zval pointer, which contains the value returned. 
+		Normally you would use things RETVAL_STRING, RETURN_STRING etc but for array 
+		it's a bit different
+	*/
+	array_init(return_value);
+
+	/*
+		The signature changed between PHP 5.2 and PHP 5.3.
+		If you are not inclined to support PHP 5.2 just use the newer version.
+		
+		
+	*/
+#if (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION < 3)
+	zend_hash_apply_with_arguments(Z_ARRVAL_P(array), (apply_func_args_t) s_get_element_type, 1, return_value);
+#else
+	/*
+		The arguments are straight-forward:
+		First pass the array zval * to Z_ARRVAL_P which gives the HashTable member
+		Next is TSRMLS_CC, I think this stands for Thread-Safe Resource Manager Local Storage Comma Call. This is for handling 
+		ZTS. The TSRMLS_CC version expands to a comma before it.
+		Next comes our callback function and indication that we pass one extra argument and that argument is the return_value
+	*/
+	zend_hash_apply_with_arguments(Z_ARRVAL_P(array) TSRMLS_CC, (apply_func_args_t) s_get_element_type, 1, return_value);
+#endif
 }
 /* }}} */
 
@@ -159,6 +302,13 @@ ZEND_BEGIN_ARG_INFO_EX(extsample_getname_args, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 /*
+	Argument info for getname
+*/
+ZEND_BEGIN_ARG_INFO_EX(extsample_arrayvaluetypes_args, 0, 0, 1)
+	ZEND_ARG_INFO(0, array)
+ZEND_END_ARG_INFO()
+
+/*
   Declare methods for our extsample class.
 */
 static
@@ -168,6 +318,8 @@ zend_function_entry php_extsample_class_methods[] =
 	PHP_ME(extsample, __construct, extsample_construct_args,   ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
 	/* Other methods below */
 	PHP_ME(extsample, getname, extsample_getname_args,   ZEND_ACC_PUBLIC)
+	/* For this method we don't need the internal structure so mark it as static */
+	PHP_ME(extsample, arrayvaluetypes, extsample_arrayvaluetypes_args,   ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
 	{ NULL, NULL, NULL }
 };
 
